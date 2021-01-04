55050aa023d24824bf08228fa154f2243840b146
Rii amri <39664310+riiamri23@users.noreply.github.com>
2021-01-04 15:13:47 +0700

    testing

--- charge/index.php
+++ charge/index.php
@@ -29,7 +29,7 @@ $charge_result = chargeAPI($api_url, $server_key, $request_body);
 // set the response http status code
 http_response_code($charge_result['http_code']);
 // then print out the response body
-echo $charge_result['body']->redirect_url;
+print($charge_result['body']);
 
 /**
  * call charge API using Curl
