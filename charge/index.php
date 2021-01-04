46babe436958ccb715cfc0286a45b105698ea3a1
Rii amri <39664310+riiamri23@users.noreply.github.com>
2021-01-04 10:59:21 +0700

    testing

--- charge/index.php
+++ charge/index.php
@@ -31,8 +31,7 @@ echo $charge_result['body'];
 
 
 function chargeAPI($api_url, $server_key, $request_body){
-
-  return 'hello';
+  return $request_body;
   $ch = curl_init();
   $curl_options = array(
     CURLOPT_URL => $api_url,
